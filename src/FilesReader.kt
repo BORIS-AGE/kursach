87c91dbb116c3ca6ca91041b020c380a63e80f89
Shrek <b.shalabanov@pnlfin.tech>
2021-05-27 22:40:32 +0300

    multithreading added

--- src/FilesReader.kt
+++ src/FilesReader.kt
@@ -1,14 +1,36 @@
 import java.io.IOException
 import java.nio.file.Files
+import java.nio.file.Path
 import java.nio.file.Paths
 
 class FilesReader(private val dir: String, private val fileStartIndex: Int, private val fileCount: Int) {
 
     fun readAllFiles(): ArrayList<String> {
-        val fileList = arrayListOf<String>()
         val path = Paths.get(dir)
+        val filesForEachThread: Int = fileCount / THREAD_COUNT
+        val resList = ArrayList<String>()
+        for (i in 1..THREAD_COUNT) {
+            var temp = 0
+            val thread = Thread {
+                resList.addAll(getFileList(path, fileStartIndex + temp, filesForEachThread))
+            }
+            thread.start()
+            thread.join()
+            temp += filesForEachThread
+        }
+
+        val leftItems = fileCount % THREAD_COUNT
+        if (leftItems != 0) {
+            resList.addAll(getFileList(path, fileStartIndex + fileCount - leftItems, leftItems))
+        }
+
+        return resList
+    }
+
+    private fun getFileList(path: Path, startIndex: Int, limit: Int): ArrayList<String> {
+        val fileList = arrayListOf<String>()
         try {
-            Files.walk(path).skip(fileStartIndex.toLong()).limit(fileCount.toLong()).forEach { filePath ->
+            Files.walk(path).skip(startIndex.toLong()).limit(limit.toLong()).forEach { filePath ->
                 filePath.toFile().let { file ->
                     if (!file.isDirectory) {
                         fileList.add(file.absolutePath)
--- src/Main.kt
+++ src/Main.kt
@@ -1,40 +1,71 @@
-import java.util.concurrent.ConcurrentHashMap
-import java.util.concurrent.CopyOnWriteArrayList
+import java.io.BufferedReader
 import java.io.File
 import java.io.FileReader
-import java.io.BufferedReader
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.CountDownLatch
 
 val invertedIndexMap = ConcurrentHashMap<String, ArrayList<String>>()
+val allFilesArray = ArrayList<String>()
+
+const val startIndex = 200 //(12500 / 50) * (33 + 30 - 1)
+const val THREAD_COUNT = 5
+val countDownLatch = CountDownLatch(THREAD_COUNT)
 
 fun main(args: Array<String>) {
-    val allFilesArray = ArrayList<String>()
-    val startIndex = 200 //(12500 / 50) * (33 + 30 - 1)
-    allFilesArray.addAll(FilesReader("res/test/neg/", startIndex, 250).readAllFiles())
-    allFilesArray.addAll(FilesReader("res/test/pos/", startIndex, 250).readAllFiles())
-    allFilesArray.addAll(FilesReader("res/train/pos/", startIndex, 250).readAllFiles())
-    allFilesArray.addAll(FilesReader("res/train/neg/", startIndex, 250).readAllFiles())
-    allFilesArray.addAll(FilesReader("res/train/unsup/", startIndex, 1000).readAllFiles())
+    collectAllFiles()
+    substringForAllIndexes()
+    countDownLatch.await()
 
-    for (fileDir: String in allFilesArray) {
-        substringForAllIndexes(fileDir)
-    }
     println(search("Creative").joinToString { "$it\n" })
 }
 
 fun search(searchValue: String): List<String> =
     invertedIndexMap.filter { it.key.equals(searchValue, true) }.flatMap { it.value }
 
-fun substringForAllIndexes(dir: String) {
-    val file = File(dir)
-    val reader = BufferedReader(FileReader(file))
-    var line = reader.readLine()
-    while (line != null) {
-        for (word in line.split("\\W+".toRegex()).toTypedArray()) {
-            invertedIndexMap.putIfAbsent(word, arrayListOf())
-            invertedIndexMap[word].apply {
-                this?.add(dir)
-            }
+fun substringForAllIndexes() {
+    val fileCountForEachThread: Int = allFilesArray.size / THREAD_COUNT
+
+    for (i in 0 until THREAD_COUNT) {
+        val thread = Thread {
+            createInvertedIndexMap(
+                allFilesArray.subList(
+                    i * fileCountForEachThread,
+                    i * fileCountForEachThread + fileCountForEachThread
+                )
+            )
+            countDownLatch.countDown()
         }
-        line = reader.readLine()
+        thread.start()
+        thread.join()
     }
+
+    val leftItems = allFilesArray.size % THREAD_COUNT
+    if (leftItems != 0) {
+        createInvertedIndexMap(allFilesArray.subList(allFilesArray.size - leftItems, allFilesArray.size))
+    }
+}
+
+fun createInvertedIndexMap(filesList: List<String>) {
+    for (dir: String in filesList) {
+        val file = File(dir)
+        val reader = BufferedReader(FileReader(file))
+        var line = reader.readLine()
+        while (line != null) {
+            for (word in line.split("\\W+".toRegex()).toTypedArray()) {
+                invertedIndexMap.putIfAbsent(word, arrayListOf())
+                invertedIndexMap[word].apply {
+                    this?.add(dir)
+                }
+            }
+            line = reader.readLine()
+        }
+    }
+}
+
+fun collectAllFiles() {
+    allFilesArray.addAll(FilesReader("res/test/neg/", startIndex, 250).readAllFiles())
+    allFilesArray.addAll(FilesReader("res/test/pos/", startIndex, 250).readAllFiles())
+    allFilesArray.addAll(FilesReader("res/train/pos/", startIndex, 250).readAllFiles())
+    allFilesArray.addAll(FilesReader("res/train/neg/", startIndex, 250).readAllFiles())
+    allFilesArray.addAll(FilesReader("res/train/unsup/", startIndex, 1000).readAllFiles())
 }
