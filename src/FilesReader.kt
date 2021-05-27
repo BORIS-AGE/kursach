cadf6ba7cb68122e9dddc7c237b78ae356896b3f
Shrek <b.shalabanov@pnlfin.tech>
2021-05-27 21:50:07 +0300

    main part finished

--- src/FilesReader.kt
+++ src/FilesReader.kt
@@ -2,15 +2,15 @@ import java.io.IOException
 import java.nio.file.Files
 import java.nio.file.Paths
 
-class FilesReader(private val dir: String, private var fileStartIndex: Int) {
+class FilesReader(private val dir: String, private val fileStartIndex: Int, private val fileCount: Int) {
 
     fun readAllFiles(): ArrayList<String> {
         val fileList = arrayListOf<String>()
         val path = Paths.get(dir)
         try {
-            Files.walk(path).forEach {
-                path.toFile().let { file ->
-                    if (!file.isDirectory && fileStartIndex-- == 0) {
+            Files.walk(path).skip(fileStartIndex.toLong()).limit(fileCount.toLong()).forEach { filePath ->
+                filePath.toFile().let { file ->
+                    if (!file.isDirectory) {
                         fileList.add(file.absolutePath)
                     }
                 }
--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,40 @@
-fun main(args : Array<String>) {
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.CopyOnWriteArrayList
+import java.io.File
+import java.io.FileReader
+import java.io.BufferedReader
 
+val invertedIndexMap = ConcurrentHashMap<String, ArrayList<String>>()
+
+fun main(args: Array<String>) {
+    val allFilesArray = ArrayList<String>()
+    val startIndex = 200 //(12500 / 50) * (33 + 30 - 1)
+    allFilesArray.addAll(FilesReader("res/test/neg/", startIndex, 250).readAllFiles())
+    allFilesArray.addAll(FilesReader("res/test/pos/", startIndex, 250).readAllFiles())
+    allFilesArray.addAll(FilesReader("res/train/pos/", startIndex, 250).readAllFiles())
+    allFilesArray.addAll(FilesReader("res/train/neg/", startIndex, 250).readAllFiles())
+    allFilesArray.addAll(FilesReader("res/train/unsup/", startIndex, 1000).readAllFiles())
+
+    for (fileDir: String in allFilesArray) {
+        substringForAllIndexes(fileDir)
+    }
+    println(search("Creative").joinToString { "$it\n" })
+}
+
+fun search(searchValue: String): List<String> =
+    invertedIndexMap.filter { it.key.equals(searchValue, true) }.flatMap { it.value }
+
+fun substringForAllIndexes(dir: String) {
+    val file = File(dir)
+    val reader = BufferedReader(FileReader(file))
+    var line = reader.readLine()
+    while (line != null) {
+        for (word in line.split("\\W+".toRegex()).toTypedArray()) {
+            invertedIndexMap.putIfAbsent(word, arrayListOf())
+            invertedIndexMap[word].apply {
+                this?.add(dir)
+            }
+        }
+        line = reader.readLine()
+    }
 }
